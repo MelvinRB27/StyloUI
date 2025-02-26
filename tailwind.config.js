d63ed216450e456534fee31c58f0a9a7b7584ee1
Melvin Ruiz Batista <melvin@codika.net>
2025-02-26 16:22:53 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6366F1",
+        secondary: "#9333EA",
+      },
+    },
+  },
+  plugins: [],
+};
